24315ab64cff23018e36cecb9328dd3fe1afd0ed
MohammadHajigasemlou <MohammadHajigasemlou2@gmail.com>
2023-07-25 18:52:43 +0330

    proabs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int[] list=new int[3];
+for(int i=0; i<3;i++)
+{
+list[i]=int.Parse(Console.ReadLine());
+}
+for(int i=0; i<list.Length;i++)
+{
+    System.Console.WriteLine(list[i]);
+}
---
+++ abspro.cs
@@ -0,0 +1,16 @@
+abstract class strr
+{
+   int id;
+   public string name;
+   string familiname; 
+
+void print()
+{
+    System.Console.WriteLine("hello Im programmer");
+}
+
+}
+class mo:strr{
+
+    
+}
